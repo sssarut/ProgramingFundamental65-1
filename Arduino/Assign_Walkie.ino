543146b0442b637a12f60f53f76595c583133f3b
Salted_Boi <oo2018oo@Laptop-29618.localdomain>
2022-10-19 10:27:17 +0700

    Wait for testing

--- Arduino/Assign_Walkie.ino
+++ Arduino/Assign_Walkie.ino
@@ -3,7 +3,8 @@
 # define Device 5
 
 String buffer = "";
-int switch = 0;
+int switch = 1;
+String format = "TOKEN#"
 
 void	setup(void)
 {
@@ -15,7 +16,7 @@ void	loop(void)
 {
 	if (Serial.available() && switch == 0)
 	{
-		int n = Serial.read() - '0';
+		char n = Serial.read();
 		switch();
 	}
 	while (Serial.available())
@@ -35,16 +36,21 @@ void	loop(void)
 	}
 	if (switch == 1 && buffer != "")
 	{
+		strcat(format, "1#");
+		strcat(format, n);
+		strcat(format, "#");
+		strcat(format, buffer);
 		Wire.beginTransmission(Device);
 		int i = 0;
-		while (buffer[i])
+		while (format[i])
 		{
-			Wire.write(buffer[i]);
+			Wire.write(format[i]);
 			i++;
 		}
 		Wire.endTransmission();
 		switch();
-
+		format = "TOKEN#";
+		buffer = "";
 	}
 }
 
@@ -56,8 +62,58 @@ void	switch(void)
 	}
 	else if (switch == 1)
 	{
-		Wire.begin(Device);
+		Wire.begin(Device - 1);
 		Wire.onReceive(receiveEvent);
 	}
 	switch = !switch;
+}
+
+void	receiveEvent(void)
+{
+	//012345678
+	//TOKEN#0#
+	//TOKEN#1#T#message
+	While (1 < Wire.available())
+	{
+		char a = Wire.read();
+		buffer += a;
+	}
+	buffer += '\0';
+	if (buffer[6] == '0')
+	{
+		switch();
+		Wire.beginTransmission(Device);
+		int j = 0;
+		while (buffer[j])
+		{
+			Wire.write(buffer[j]);
+			j++;
+		}
+		Wire.endTransmission();
+		switch();
+	}
+	else if (buffer[6] == '1')
+	{
+		if (buffer[8] - '0' = Device - 1)
+		{
+			int p = 10;
+			while (buffer[p])
+			{
+				Serial.print(buffer[p]);
+				p++;
+			}
+			Serial.println('\n');
+			buffer = "TOKEN#0#";
+		}
+		switch();
+		Wire.beginTransmission(Device);
+		int l = 0;
+		while (buffer[l])
+		{
+			Wire.write(buffer[l]);
+			l++;
+		}
+		Wire.endTransmission();
+		switch();
+	}
 }
