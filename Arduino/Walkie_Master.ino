91e49ee29a356d739592b107ac94097b1e0dea64
Salted_Boi <oo2018oo@Laptop-29618.localdomain>
2022-10-25 13:33:44 +0700

    Change the method

---
+++ Arduino/Walkie_Master.ino
@@ -0,0 +1,88 @@
+#include<Wire.h>
+# define DEVICE 1 
+char buffer[100];
+int target = 2;
+int send;
+char c;
+
+void	setup(void)
+{
+	Wire.begin();
+	Serial.begin(9600);
+}
+
+void	loop(void)
+{
+	delay(1000);
+	sending();
+	Wire.requestFrom(target, 90);
+	int i = 0;
+	while(Wire.available())
+	{
+		buffer[i] = Wire.read();
+		//Serial.print(c);
+		i++;
+	}
+	if(buffer[0] != '#')
+	{
+		if(buffer[0] - '0' == DEVICE)
+		{
+			i = 1
+			while(buffer[i])
+			{
+				c = buffer[i];
+				Serial.print(c);
+				i++;
+			}
+		}
+		else
+		{
+			send = buffer[0] = '0';
+			i = 1;
+			Wire.beginTransmission(send);
+			while(buffer[i])
+			{
+				Wire.write(buffer[i]);
+				i++;
+			}
+			Wire.endTransmission();
+		}
+	}
+	buffer = "";
+	if(target == 2)
+		target = 3;
+	else
+		target = 2;
+}
+
+void	sending(void)
+{
+	while(Serial.available())
+	{
+		char c = Serial.read();
+		if(c != '\n')
+		{
+			if(c == '\r')
+				continue;
+			buffer += c;
+			continue;
+		}
+		else
+		{
+			buffer += '\0';
+			if(buffer[0] >= '1' && buffer[0] <= '3')
+			{
+				send = buffer[0] - '0';
+			}
+			i = 1;
+			Wire.beginTransmission(send);
+			while(buffer[i])
+			{
+				Wire.write(buffer[i]);
+				i++;
+			}
+			Wire.endTransmission();
+		}
+	}
+	buffer = "";
+}
---
+++ Arduino/Walkie_Slave.ino
@@ -0,0 +1,5 @@
+#include <Wire.h>
+# define DEVICE 2
+
+char	buffer[100];
+
