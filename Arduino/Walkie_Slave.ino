cf689232636ba5b587c7befb53717744f7c0da2b
Salted_Boi <oo2018oo@Laptop-29618.localdomain>
2022-10-25 13:49:53 +0700

    Change the method

--- Arduino/Walkie_Slave.ino
+++ Arduino/Walkie_Slave.ino
@@ -1,5 +1,50 @@
 #include <Wire.h>
 # define DEVICE 2
 
-char	buffer[100];
+char	buffer[100] = "";
+char	c;
 
+void	setup(void)
+{
+	Wire.begin(DEVICE);
+	Serial.begin(9600);
+	Wire.onRequest(event);
+}
+
+void	loop(void)
+{
+	while(Serial.available())
+	{
+		char c = Serial.read();
+		if(c != '\n')
+		{
+			if(c == '\r')
+				continue;
+			buffer += c;
+			continue;
+		}
+		else
+		{
+			buffer += '\0';
+		}
+	}
+}
+
+void	event(void)
+{
+	if(buffer != "")
+	{
+		int t = 0;
+		while(buffer[t])
+		{
+			c = buffer[t];
+			Wire.write(c);
+			t++;
+		}
+	}
+	else if(buffer == "")
+		Wire.write("#");
+	else
+		Wire.write("1Error");
+	buffer = "";
+}
